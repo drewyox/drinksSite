2c6447a37534f846d6eeb854450e9621c55dcbc0
Drew Yoximser and Nelia Terrazas <drew@>
2019-03-06 15:34:04 -0800

    add blowups and js to whiskey drinks

--- src/index.js
+++ src/index.js
@@ -7,9 +7,41 @@ $(document).ready(function(){
 
 $(".button").click(function(){
     $(".ingredient1").addClass("fiveID1");
-    $(".ingredient2").addClass();
-    $(".ingredient2").addClass();
-    $(".ingredient2").addClass();
-    $(".ingredient2").addClass();
+    $(".ingredient2").addClass("fiveID2");
+    $(".ingredient3").addClass("fiveID3");
+    $(".ingredient4").addClass("fiveID4");
+    $(".ingredient5").addClass("fiveID5");
+    $(".drink2W").addClass("blowUps");
+    $(".drink3W").addClass("blowUps");
+    $(".drink4W").addClass("blowUps");
   })
-})
+
+$(".buttonMan").click(function(){
+    $(".man-1").addClass("threeID1");
+    $(".man-2").addClass("threeID2");
+    $(".man-3").addClass("threeID3");
+    $(".drink1W").addClass("blowUps");
+    $(".drink2W").addClass("blowUps");
+    $(".drink4W").addClass("blowUps");
+  })
+
+$(".buttonSou").click(function(){
+    $(".w-s1").addClass("threeID1");
+    $(".w-s2").addClass("threeID2");
+    $(".w-s3").addClass("threeID3");
+    $(".drink1W").addClass("blowUps");
+    $(".drink3W").addClass("blowUps");
+    $(".drink4W").addClass("blowUps");
+  })
+$(".buttonJ").click(function(){
+    $(".m-j1").addClass("fiveID1");
+    $(".m-j2").addClass("fiveID2");
+    $(".m-j3").addClass("fiveID3");
+    $(".m-j4").addClass("fiveID4");
+    $(".m-j5").addClass("fiveID5");
+    $(".drink2W").addClass("blowUps");
+    $(".drink3W").addClass("blowUps");
+    $(".drink1W").addClass("blowUps");
+  })
+
+});
