74827328e003801343c591cc8485c214df9e4eb0
Drew Yoxsimer and Nelia Terrazas <drew@>
2019-03-05 16:45:31 -0800

    final push

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,10 +1,24 @@
-$(window).bind('scroll',function(e){
-    parallaxScroll();
-});
 
-function parallaxScroll(){
-    var scrolled = $(window).scrollTop();
-    $('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
-    $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
-    $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// $(window).bind('scroll',function(e){
+//   parallaxScroll();
+// });
+//
+// function parallaxScroll(){
+//   var scrolled = $(window).scrollTop();
+//   $('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
+//   $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+//   $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
+// }
